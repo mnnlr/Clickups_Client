c18028cebd9d65d3ea085e760d6f04a425ebe75a
RohitKumar9690 <rpal46515@gmail.com>
2024-09-02 14:43:01 +0530

    Update customAxios.js

--- src/CustomAxios/customAxios.js
+++ src/CustomAxios/customAxios.js
@@ -1,7 +1,17 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000'
+// const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://clickups-server.onrender.com'
 
 export default axios.create({
     baseURL: BASE_URL,
 });
+
+export const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    withCredentials: true,
+});
+ 
