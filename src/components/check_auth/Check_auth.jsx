f614f789c0532f8cdd0976813b5efb4b09c0a4e6
mern-mnnlr-rushi <mern.mnnlr.rushipanchal@gmail.com>
2024-10-16 18:26:57 +0530

    small update

--- src/components/check_auth/Check_auth.jsx
+++ src/components/check_auth/Check_auth.jsx
@@ -19,7 +19,7 @@ const Check_auth = () => {
         const { data } = await axiosPrivate("/api/authenticate", {
           headers: {
             "Content-Type": "Application/json",
-            authorization: `Bearer ${userData?.user?.token}`,
+            // authorization: `Bearer ${userData?.user?.token}`,
           },
           withCredentials: true,
         });
@@ -31,7 +31,7 @@ const Check_auth = () => {
         }
       } catch (err) {
         navigate("/signin");
-        return console.log("error on authentication", err.message);
+        return console.log("error on authentication", err);
       }
     };
     isMounted && response();
