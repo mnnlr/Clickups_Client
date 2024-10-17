13c40db7971765f0c203297b92e65210d96b5539
Rahul_Pharthyal_MNNLR <mern.mnnlr.rahulpharthyal@gmail.com>
2024-10-17 17:08:29 +0530

    change the base Url

--- src/CustomAxios/customAxios.js
+++ src/CustomAxios/customAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/'
-// const BASE_URL = 'https://clickups-server.onrender.com'
+// const BASE_URL = 'http://localhost:5000/'
+const BASE_URL = 'https://clickups-server.onrender.com'
 
 export default axios.create({
     baseURL: BASE_URL,
