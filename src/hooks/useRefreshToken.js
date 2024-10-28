5f2d53bad7aebb9627be512b3f8ad3d3d09a7eea
mern-mnnlr-rushi <mern.mnnlr.rushipanchal@gmail.com>
2024-10-28 20:32:03 +0530

    updated teh actions access in project page
    now admin can access all actions in all project and the user can access only the project that he created.

--- src/hooks/useRefreshToken.js
+++ src/hooks/useRefreshToken.js
@@ -1,4 +1,4 @@
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import customAxios from '../CustomAxios/customAxios';
 import { logoutSuccess, setUser } from '../redux/authentication/loginSlice';
 import { useNavigate } from 'react-router-dom';
@@ -7,17 +7,17 @@ const useRefreshToken = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const refresh = async () => {
-        try{
+        try {
             const response = await customAxios.get("/api/v1/refresh-token", {
                 withCredentials: true,
             });
             console.log('this is refresh Token', response);
             dispatch(setUser(response.data));
-            return response.data.accessToken;
 
-        }catch(error){
+            return response.data.accessToken;
+        } catch (error) {
             console.log('this is error from refreshing Page', error);
-            if(error.response.status === 401){
+            if (error.response.status === 401) {
                 navigate('/signin');
             }
         }
--- src/views/Project.jsx
+++ src/views/Project.jsx
@@ -3,9 +3,7 @@ import CreateProjectModal from '../components/Models/CreateProjectModal';
 import EditProjectModal from '../components/Models/EditProjectModal';
 import AddMembersModal from '../components/Models/AddMemberModal';
 import Cookies from 'js-cookie';
-import { axiosPrivate } from '../CustomAxios/customAxios';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import DeleteConfirmationModal from '../components/Models/DeleteConfirmModel';
 import { showToast } from '../components/Toastconfig';
 import useGetMembers from '../project-utils-and-hooks/useGetMembers';
@@ -15,6 +13,7 @@ import useUpdateProject from '../project-utils-and-hooks/useUpdateProject';
 import useCreateProject from '../project-utils-and-hooks/useCreateProject';
 import useHandleDelete from '../project-utils-and-hooks/useHandleDelete';
 import handleAddMembersSubmit from '../project-utils-and-hooks/handleAddMembersSubmit';
+import { useSelector } from 'react-redux';
 
 const Project = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -35,11 +34,11 @@ const Project = () => {
     status: '',
   });
 
+
   const navigate = useNavigate()
   const [currentProject, setCurrentProject] = useState(null);
-
-  // User Token
-  const token = Cookies.get("User");
+  const user = useSelector((state) => state.login.user);
+  // console.log("user: ", user);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => {
@@ -108,13 +107,12 @@ const Project = () => {
       setSelectedTeams([...selectedTeams, teamToAdd]);
     }
     // console.log("selectedTeams: " + selectedTeams)
-
   };
   // console.log("selected members and teams: " + selectedMembers, selectedTeams)
 
 
   const handleAddMember = (memberId) => {
-    console.log("selectedMembers: " + JSON.stringify(selectedMembers));
+    // console.log("selectedMembers: " + JSON.stringify(selectedMembers));
     if (!selectedMembers?.some(member => member.id === memberId)) {
       console.log("selectedMembers: " + selectedMembers);
       const memberToAdd = availableMembers.find(member => member.id === memberId);
@@ -151,7 +149,7 @@ const Project = () => {
   };
 
   const handleDeleteToProject = (project) => {
-    setprojectTodelete(project.id); // Make sure project.id is passed
+    setprojectTodelete(project.id);
     setdeleteModel(true);
   };
 
@@ -175,6 +173,8 @@ const Project = () => {
   const { updateProject } = useUpdateProject({ projects, setProjects, closeEditModal, showToast, projectData, currentProject, selectedTeams });
   const { createProject } = useCreateProject({ projects, projectData, selectedTeams, setProjects, closeModal });
   const { handleDelete } = useHandleDelete({ projectTodelete, setProjects, setdeleteModel, setprojectTodelete });
+
+  // console.log("projects: ", projects);
 
   return (
     <div className="relative p-6 bg-gray-100 dark:bg-gray-900 h-screen ml-16 overflow-auto md:ml-16 lg:ml-20 pt-20">
@@ -236,7 +236,7 @@ const Project = () => {
               <th className="py-3 px-6 text-left">Owner</th>
               <th className="py-3 px-6 text-left">Due Date</th>
               <th className="py-3 px-6 text-left">Status</th>
-              <th className="py-3 px-6 text-right">More Actions</th>
+              <th className="py-3 px-6 text-right">Actions</th>
             </tr>
           </thead>
           <tbody className="text-gray-600 dark:text-gray-300 text-sm font-light">
@@ -264,42 +264,46 @@ const Project = () => {
                 <td className="py-3 px-6 text-left">{new Date(project.dueDate).toLocaleDateString() || ''}</td>
                 <td className="py-3 px-6 text-left">{project.status}</td>
                 <td className="py-2 px-3 text-center space-x-3 flex flex-row items-end justify-end">
-                  <button
-                    onClick={() => handleDeleteToProject(project)}
-                    className="bg-red-500 font-mono font-bold text-white px-3 py-1 rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 dark:focus:ring-red-600 mr-5 duration-300 ease-in-out whitespace-nowrap"
-                  >
-                    Delete
-                  </button>
-                  <button
-                    onClick={() => openEditModal(project)}
-                    className="bg-blue-500 text-white text-center px-3 py-1 duration-300 ease-in-out mr-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 whitespace-nowrap"
-                  >
-                    Edit
-                  </button>
+                  {(user.role === "Admin" || project.owner === user._id) && (
+                    <>
+                      <button
+                        onClick={() => handleDeleteToProject(project)}
+                        className="bg-red-500 font-mono font-bold text-white px-3 py-1 rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-300 dark:focus:ring-red-600 mr-5 duration-300 ease-in-out whitespace-nowrap"
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={() => openEditModal(project)}
+                        className="bg-blue-500 text-white text-center px-3 py-1 duration-300 ease-in-out mr-2 rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-600 whitespace-nowrap"
+                      >
+                        Edit
+                      </button>
 
-                  <button
-                    onClick={() => openAddMembersModal(project)}
-                    className="bg-green-500 text-white px-3 py-1 duration-300 ease-in-out rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 dark:focus:ring-green-600 mr-2 whitespace-nowrap"
-                  >
-                    Add Members
-                  </button>
+                      <button
+                        onClick={() => openAddMembersModal(project)}
+                        className="bg-green-500 text-white px-3 py-1 duration-300 ease-in-out rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300 dark:focus:ring-green-600 mr-2 whitespace-nowrap"
+                      >
+                        Add Members
+                      </button>
 
-                  <button
-                    onClick={() => navigate(`/dashboard/${project.id}`)}
-                    className="bg-indigo-500 text-white flex flex-row px-3 py-1 mr-2 rounded-lg shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300 transition duration-150 ease-in-out whitespace-nowrap"
-                  >
-                    View
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 ml-1.5">
-                      <path fill-rule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
-                    </svg>
-                  </button>
+                      <button
+                        onClick={() => navigate(`/dashboard/${project.id}`)}
+                        className="bg-indigo-500 text-white flex flex-row px-3 py-1 mr-2 rounded-lg shadow-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-300 transition duration-150 ease-in-out whitespace-nowrap"
+                      >
+                        View
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 ml-1.5">
+                          <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                        </svg>
+                      </button>
+                    </>
+                  )}
                   <button
                     onClick={() => navigate(`/projects/${project.id}/sprints`)}
                     className="bg-blue-500 text-white px-3 py-1 inline-flex rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 transition duration-150 ease-in-out whitespace-nowrap"
                   >
                     Go to Task
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 ml-1.5">
-                      <path fill-rule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 ml-1.5">
+                      <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                     </svg>
                   </button>
                 </td>
