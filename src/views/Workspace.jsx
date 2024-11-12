1f78114d938c4cab7012b347e674639f9fc91aed
Rohit Kumar <rpal46515@gmail.com>
2024-11-12 15:44:39 +0530

    updatedworkspace page

--- src/views/Workspace.jsx
+++ src/views/Workspace.jsx
@@ -1,55 +1,155 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import SearchBox from './Search';
 import AddMembersModal from '../components/Models/AddMemberModal';
+import customAxios from '../CustomAxios/customAxios';
+import Cookies from 'js-cookie';
 import Modal from '../components/Models/Modal';
+import { useSelector } from 'react-redux';
+import { MdModeEditOutline, MdOutlineDeleteForever } from "react-icons/md";
+import { FaUserPlus } from "react-icons/fa6";
+
 
 const AllWorkspaces = () => {
   const [workspaces, setWorkspaces] = useState([
-    { _id: 1, name: 'Nike', type: 'Team', createdAt: 'Oct 17, 2019' },
-    { _id: 2, name: 'Pepsi', type: 'Personal', createdAt: 'Feb 5, 2020' },
-    { _id: 3, name: 'Disney', type: 'Team', createdAt: 'Jul 12, 2018' },
-    { _id: 4, name: 'Apple', type: 'Personal', createdAt: 'Dec 1, 2021' },
-    { _id: 5, name: 'Tesla', type: 'Team', createdAt: 'Jan 20, 2022' },
+    { _id: 1, workspaceName: 'Nike', type: 'Team', createdAt: 'Oct 17, 2019',workspaceCreatedBy:"Manoj" },
+    { _id: 2, workspaceName: 'Pepsi', type: 'Personal', createdAt: 'Feb 5, 2020' },
+    { _id: 3, workspaceName: 'Disney', type: 'Team', createdAt: 'Jul 12, 2018' },
+    { _id: 4, workspaceName: 'Apple', type: 'Personal', createdAt: 'Dec 1, 2021' },
+    { _id: 5, workspaceName: 'Tesla', type: 'Team', createdAt: 'Jan 20, 2022' },
   ]);
-  
   const [isAddMembersModalOpen, setIsAddMembersModalOpen] = useState(false);
   const [newWorkspaceName, setNewWorkspaceName] = useState('');
   const [workspaceType, setWorkspaceType] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [search, setSearch] = useState('');
   const [filterWorkspace, setFilterWorkspace] = useState([]);
-
+  const { user } = useSelector((store) => store.login);
   const navigate = useNavigate();
-
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const location = useLocation();
+  const token = Cookies.get("User");
+  
+  const [isDropdownOpen, setIsDropdownOpen] = useState(null);
+  
+  const toggleDropdown = (index, e) => {
+    e.stopPropagation();
+    setIsDropdownOpen(isDropdownOpen === index ? null : index);
+  };
+  
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [workspaceToEdit, setWorkspaceToEdit] = useState(null);
+  
+  const handleEditWorkspace = (workspace) => {
+    setWorkspaceToEdit(workspace);
+    setNewWorkspaceName(workspace.workspaceName);
+    setWorkspaceType(workspace.type);
+    setIsEditMode(true);
+    setIsModalOpen(true);
+  };
 
   useEffect(() => {
-    setFilterWorkspace(
-      workspaces.filter((space) =>
-        space.name.toLowerCase().includes(search.toLowerCase())
-      )
-    );
-  }, [search, workspaces]);
+    fetchWorkspaces()
+  }, []);
 
-  const addWorkspace = () => {
-    if (newWorkspaceName.trim() && workspaceType) {
-      const newWorkspace = { 
-        name: newWorkspaceName, 
-        type: workspaceType, 
-        createdAt: new Date().toLocaleDateString() 
+  
+  useEffect(() => {
+    setIsDropdownOpen(null);
+  }, [location]);
+
+ useEffect(() => {
+    if (Array.isArray(workspaces)) {
+      setFilterWorkspace(
+        workspaces.filter((space) =>
+          space.workspaceName.toLowerCase().includes(search.toLowerCase())
+        )
+      );
+    }
+  }, [search]);
+
+
+  const handleDeleteWorkspace = async (workspaceId) => {
+    try {
+      const response = await customAxios.delete(`/api/workspaces/${workspaceId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      if (response.status === 200) {
+        setWorkspaces((prevWorkspaces) =>
+          prevWorkspaces.filter((workspace) => workspace._id !== workspaceId)
+        );
+        console.log('Workspace deleted successfully');
+      }
+    } catch (error) {
+      console.error('Error deleting workspace:', error);
+    }
+  };
+
+
+
+  const fetchWorkspaces = async () => {
+    try {
+      const response = await customAxios.get('/api/workspaces/', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`,
+        },
+      });
+      
+      if (response.status === 200) {
+       
+        console.log(response.data.data);
+      }
+    } catch (error) {
+      console.error('Error fetching workspaces:', error);
+    }
+  };
+  
+
+  const openModal = () => setIsModalOpen(true);
+
+  const closeModal = () => {
+    setNewWorkspaceName('');
+    setWorkspaceType('');
+    setIsModalOpen(false);
+  };
+
+ 
+  
+
+  const addWorkspace = async () => {
+    if (newWorkspaceName.trim()) {
+      const newWorkspace = {
+        workspaceName: newWorkspaceName,
+        workspaceCreatedBy: user._id
       };
-      setWorkspaces([...workspaces, newWorkspace]);
-      setNewWorkspaceName('');
-      setWorkspaceType('');
-      closeModal();
+
+      try {
+        const response = await customAxios.post('/api/workspaces/', newWorkspace, {
+          headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          setWorkspaces((prevWorkspaces) => [...prevWorkspaces, response.data]);
+          setNewWorkspaceName('');
+          setWorkspaceType('');
+          closeModal();
+        } else {
+          throw new Error('Failed to add workspace');
+        }
+      } catch (error) {
+        console.error('Error adding workspace:', error);
+      }
     }
   };
 
   const switchWorkspace = (workspace) => {
     navigate(`/workspace/${workspace._id}/${workspace.type}`, {
-      state: { workspaceName: workspace.name },
+      state: { workspaceName: workspace.workspaceName },
     });
   };
 
@@ -66,29 +166,54 @@ const AllWorkspaces = () => {
     setIsAddMembersModalOpen(false);
   };
 
+
+  const handleSaveChanges = async () => {
+    if (workspaceToEdit) {
+      const updatedWorkspace = {
+        workspaceName: newWorkspaceName
+      };
+
+      try {
+        const response = await customAxios.put(`/api/workspaces/${workspaceToEdit._id}`, updatedWorkspace, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          setWorkspaces((prevWorkspaces) =>
+            prevWorkspaces.map(workspace =>
+              workspace._id === workspaceToEdit._id ? response.data : workspace
+            )
+          );
+          closeModal();
+        } else {
+          throw new Error('Failed to update workspace');
+        }
+      } catch (error) {
+        console.error('Error updating workspace:', error);
+      }
+    }
+  };
+
   return (
-    <div className={`p-6 bg-gray-100 dark:bg-gray-900 dark:text-white h-screen ml-16 overflow-auto md:ml-16 lg:ml-20`}>
+    <div className="p-6 bg-gray-100 dark:bg-gray-900 dark:text-white h-screen ml-16 overflow-auto md:ml-16 lg:ml-20">
       <div className="py-16">
         <div className="flex justify-between items-center mb-6">
           <h1 className="text-2xl font-semibold">All Workspaces</h1>
           <div className="flex items-center gap-5">
             <SearchBox search={search} handleSearch={handleSearch} />
-            <button
-              className="bg-blue-500 h-10 text-white px-4 py-0 rounded"
-              onClick={AddMember}
-            >
-              Manage Team
-            </button>
           </div>
         </div>
-        
+
         <div className="p-4">
           <div
-            className="relative w-36 h-36 bg-gray-100 dark:bg-gray-800 flex flex-col items-center justify-center rounded-lg shadow cursor-pointer"
+            className="relative w-36 h-36 bg-gray-100 dark:bg-gray-800 flex flex-col items-center justify-center rounded-lg shadow cursor-pointer transition-all duration-300 transform hover:scale-105 hover:shadow-xl"
             onClick={openModal}
           >
             <div className="text-center">
-              <div className="relative w-32 h-32 bg-gray-100 dark:bg-gray-800 flex flex-col items-center justify-center rounded-lg shadow cursor-pointer hover:shadow-sm shadow-black">
+              <div className="relative w-32 h-32 bg-gray-100 dark:bg-gray-800 flex flex-col items-center justify-center rounded-lg shadow cursor-pointer hover:shadow-sm">
                 <div className="text-4xl text-gray-400 dark:text-gray-200 mb-2">+</div>
                 <p className="text-gray-600 dark:text-gray-400">Create a workspace</p>
               </div>
@@ -99,77 +224,155 @@ const AllWorkspaces = () => {
         <div className="flex items-center justify-center py-3">
           <h1 className="text-2xl font-bold">Workspaces</h1>
         </div>
-        <div className="grid grid-cols-3 gap-3">
+        <div className="grid grid-cols-3 gap-6">
           {filterWorkspace.map((workspace, index) => (
             <div
-              key={index}
-              className="bg-white dark:bg-gray-800 rounded-lg p-6 relative cursor-pointer transition-colors duration-200 hover:bg-gray-50 dark:hover:bg-gray-700"
-              onClick={() => switchWorkspace(workspace)}
+              key={workspace._id}
+              className="bg-white dark:bg-gray-800 rounded-lg p-6 relative cursor-pointer transition-all duration-300 ease-in-out hover:bg-gray-50 dark:hover:bg-gray-700 transform hover:scale-105 hover:shadow-lg"
+
             >
               <span className="absolute top-2 left-2 bg-blue-100 text-blue-500 text-xs px-2 py-1 rounded">
                 {workspace.type}
               </span>
-              <div className="flex items-center mb-4">
+              <div className="flex items-center mb-4" onClick={() => switchWorkspace(workspace)}>
                 <img
-                  alt={`${workspace.name} logo`}
+                  alt={`${workspace.workspaceName} logo`}
                   className="rounded-full mr-4"
                   height="40"
                   src="https://storage.googleapis.com/a1aa/image/WgMPA8Y3tkKQJ9Jl5DaJjDTZfgT1I1unvjelHl6Pz22lc7tTA.jpg"
                   width="40"
                 />
-                <h2 className="text-lg font-semibold text-gray-900 dark:text-white">{workspace.name}</h2>
+                <h2 className="text-lg font-semibold text-gray-900 dark:text-white">{workspace.workspaceName}</h2>
               </div>
-              <p className="text-gray-400 dark:text-gray-500 text-sm mb-4">Created: {workspace.createdAt}</p>
+              <p className="text-sm text-gray-600 dark:text-gray-300 mb-4">Created: {workspace.workspaceCreatedBy}</p>
+              <div className="flex items-center mb-4">
+                <img
+                  alt="user avatar"
+                  className="rounded-full"
+                  height="20"
+                  src="https://storage.googleapis.com/a1aa/image/PzjG2aSlcALqKB1k2qYk0llOfaWaSfwftL3QfPCYRIE99lAPB.jpg"
+                  width="20"
+                />
+                <img
+                  alt="user avatar"
+                  className="rounded-full -ml-1"
+                  height="20"
+                  src="https://storage.googleapis.com/a1aa/image/PzjG2aSlcALqKB1k2qYk0llOfaWaSfwftL3QfPCYRIE99lAPB.jpg"
+                  width="20"
+                />
+                <img
+                  alt="user avatar"
+                  className="rounded-full -ml-1"
+                  height="20"
+                  src="https://storage.googleapis.com/a1aa/image/PzjG2aSlcALqKB1k2qYk0llOfaWaSfwftL3QfPCYRIE99lAPB.jpg"
+                  width="20"
+                />
+                <img
+                  alt="user avatar"
+                  className="rounded-full -ml-1"
+                  height="20"
+                  src="https://storage.googleapis.com/a1aa/image/PzjG2aSlcALqKB1k2qYk0llOfaWaSfwftL3QfPCYRIE99lAPB.jpg"
+                  width="20"
+                />
+                <span className="text-gray-400 ml-2">+4 </span>
+              </div>
+              <div className="absolute top-2 right-2">
+                <button
+                  aria-haspopup="true"
+                  aria-expanded={isDropdownOpen === index ? 'true' : 'false'}
+                  className="text-gray-500 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition-all duration-300"
+                  onClick={(e) => toggleDropdown(index, e)}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 9l6 6 6-6" />
+                  </svg>
+                </button>
+
+                {isDropdownOpen === index && (
+                  <div className="absolute right-0 mt-2 w-12 bg-white dark:bg-gray-800 shadow-lg rounded-lg transition-all duration-300 opacity-100">
+                    <ul>
+                      <li>
+                        <button
+                          onClick={() => handleEditWorkspace(workspace)}
+                          className="block px-4 py-2 text-sm text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 items-center"
+                        >
+                          <MdModeEditOutline size={20} />
+                        </button>
+                      </li>
+                      <li>
+                        <button
+                          onClick={() => AddMember(workspace)}
+                          className="block px-4 py-2 text-sm text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                        >
+                          <FaUserPlus size={20} />
+                        </button>
+                      </li>
+                      <li>
+                        <button
+                          onClick={() => handleDeleteWorkspace(workspace._id)}
+                          className="block px-4 py-2 text-sm text-gray-900 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                        >
+                          <MdOutlineDeleteForever size={20} />
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
             </div>
           ))}
+
         </div>
 
-        {/* Create Workspace Modal */}
+        {/* Create/Edit Workspace Modal */}
         {isModalOpen && (
-          <Modal title="Create a New Workspace" onClose={closeModal}>
+          <Modal
+            title={isEditMode ? "Edit Workspace" : "Create a New Workspace"}
+            onClose={closeModal}
+          >
             <form>
               <div className="mb-4">
-                <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="workspace-name">
+                <label className="block text-gray-700 dark:text-gray-300 text-sm font-bold mb-2" htmlFor="workspace-workspaceName">
                   Workspace Name
                 </label>
                 <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-white dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="workspace-name"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 dark:text-white dark:bg-gray-700 leading-tight focus:outline-none focus:shadow-outline transition-all duration-300"
+                  id="workspace-workspaceName"
                   type="text"
                   value={newWorkspaceName}
                   onChange={(e) => setNewWorkspaceName(e.target.value)}
-                  placeholder="Enter workspace name"
+                  placeholder="Enter workspace workspaceName"
                 />
               </div>
               <div className="flex space-x-4 mb-4">
                 <button
                   type="button"
                   onClick={() => setWorkspaceType('Personal')}
-                  className={`p-2 rounded-lg w-full transition-colors duration-200 ${workspaceType === 'Personal'
+                  className={`p-2 rounded-lg w-full transition-colors duration-300 ${workspaceType === 'Personal'
                     ? 'bg-blue-500 text-white'
                     : 'bg-gray-200 text-gray-600 dark:bg-gray-700 dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-gray-600'
-                  }`}
+                    }`}
                 >
                   Personal
                 </button>
                 <button
                   type="button"
                   onClick={() => setWorkspaceType('Team')}
-                  className={`p-2 rounded-lg w-full transition-colors duration-200 ${workspaceType === 'Team'
+                  className={`p-2 rounded-lg w-full transition-colors duration-300 ${workspaceType === 'Team'
                     ? 'bg-blue-500 text-white'
                     : 'bg-gray-200 text-gray-600 dark:bg-gray-700 dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-gray-600'
-                  }`}
+                    }`}
                 >
                   Team
                 </button>
               </div>
               <button
                 type="button"
-                onClick={addWorkspace}
-                className="p-2 w-full bg-blue-600 text-white rounded-lg transition-colors duration-200 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 disabled:opacity-50"
+                onClick={isEditMode ? handleSaveChanges : addWorkspace}
+                className="p-2 w-full bg-blue-600 text-white rounded-lg transition-colors duration-300 hover:bg-blue-700 dark:bg-blue-500 dark:hover:bg-blue-600 disabled:opacity-50"
                 disabled={!newWorkspaceName || !workspaceType}
               >
-                Create Workspace
+                {isEditMode ? "Save Changes" : "Create Workspace"}
               </button>
             </form>
           </Modal>
