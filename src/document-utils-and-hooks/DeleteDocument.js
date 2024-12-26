a7725d31ea656e8b40ce22b48bf9fd8e379346bf
mern-mnnlr-rushi <mern.mnnlr.rushipanchal@gmail.com>
2024-12-26 14:38:30 +0530

    updated doc delete function

--- src/document-utils-and-hooks/DeleteDocument.js
+++ src/document-utils-and-hooks/DeleteDocument.js
@@ -5,8 +5,30 @@ export default async function DeleteDocument({ workspaceId, docId }) {
     const token = Cookies.get("token");
 
     // console.log(workspaceId)
-
-    const response = await axiosPrivate.delete(`/api/workspace/documents/${docId}?workspaceId=${workspaceId}`, {
+    const cloudDelete = await axiosPrivate.delete(`/api/cloudData/${docId}`, {
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${token}`
+        }
+    });
+    console.log("cloudDelete", cloudDelete)
+    if (cloudDelete.status === 200) {
+        const response = await axiosPrivate.delete(`/api/workspace/documents/${docId}?workspaceId=${workspaceId}`, {
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `Bearer ${token}`
+            }
+        });
+        if (response.data.success) {
+            return true;
+        } else {
+            console.error("error while updating document:", response);
+            return false;
+        }
+    } else {
+        console.error("error while deleting cloudinary data:", cloudDelete)
+    }
+    /* const response = await axiosPrivate.delete(`/api/workspace/documents/${docId}?workspaceId=${workspaceId}`, {
         headers: {
             "Content-Type": "application/json",
             "authorization": `Bearer ${token}`
@@ -17,5 +39,5 @@ export default async function DeleteDocument({ workspaceId, docId }) {
     } else {
         console.error("error while updating document:", response);
         return false;
-    }
+    } */
 }
